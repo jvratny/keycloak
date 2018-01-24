016803989da7e57b52e7b876b47267d59ae4e3bf
Vojtěch Zicha <vojtech@zicha.name>
2018-01-24 09:34:27 +0100

    FIX: added boot script

---
+++ boot_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# keycloak      Startup script for the Keycloak IDM Server
+#
+# chkconfig: -  85 15
+# description:  The Keycloak Application Server
+#
+# processname:  standalone.sh
+#
+
+KEYCLOAK_HOME="/home/ew_admin/bin/keycloak/"
+IP_ADDRESS=`ip -f inet a show eth0 | grep inet | awk '{ print $2 }' | cut -d/ -f1`
+
+start() {
+    echo "Starting keycloak..."
+    
+    $KEYCLOAK_HOME/bin/standalone.sh -b $IP_ADDRESS -bprivate $IP_ADDRESS --server-config=standalone-ha.xml
+}
+
+stop() { }
+
+restart() { }
